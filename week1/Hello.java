158ed5e0c5216e1c26158d95a796dd4fd19e382e
asrim <asrimmsuwall@gmail.com>
2024-04-15 10:29:28 +0545

    Week 1 task 1 comment

--- week1/Hello.java
+++ week1/Hello.java
@@ -43,6 +43,30 @@ System.out.println("****\n");
 *********
  */
 
+ System.out.println("*********");
+ System.out.println("*********");
+ System.out.println("*********");
+ System.out.println("*********");
+
+ 
+ /*6. WAP that prints the following output:
+
+A well-formed Java program has a main method with { and } braces.
+
+A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+(But we type \" instead!)
+What is the difference between
+a ' and a "?  Or between a " and a \"?
+
+
+One is what we see when we're typing our program. The other is what appears on the "console." */
+
+System.out.println("\nA well-formed Java program has a main method with { and } braces.\n");
+System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n");
+System.out.println("(But we type \\\" instead!)\n");
+System.out.println("What is the difference between\n");
+System.out.println("a ' and a \"?  Or between a \" and a \\\"?\n");
+System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
 
     }
 }
